877977bd29a71c9f918e499c5b5908df5b24fab9
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-11-20 21:42:38 -0600

    add: connection with psql database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import knex from 'knex';
+import configuration from './knexfile.js';
+const db = knex(configuration);
 
 dotenv.config();
 const app = express();
@@ -12,12 +15,23 @@ const  PORT = process.env.PORT || 8080;
 //     credentials: true,
 // };
 
-// app.use(cors())
+app.use(cors())
 // app.use(cors(corsOptions))
 // app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("<h1>Booktale backend connected</h1>")
+// app.get('/', (req, res) => {
+//     res.send("<h1>Booktale backend connected</h1>")
+// })
+
+app.get("/", async (req, res) => {
+    try {
+        const data = await db("books").select('*');
+
+        res.json({ message: 'db connection connection success', data});
+    } catch (error){
+        console.error("error connecting to db", error)
+        res.status(500).send("ERROR CONNECTING TO DB");
+    }
 })
 
 app.listen(PORT, () => {
